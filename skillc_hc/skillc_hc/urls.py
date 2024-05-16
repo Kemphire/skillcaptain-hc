19e18705852c66337790e69228bd2757a0678808
Kartikey Shahi <kartikeyshahi@hotmail.ccom>
2024-05-16 17:38:32 +0530

    added hello view and mapped it in the urls file

--- skillc_hc/skillc_hc/urls.py
+++ skillc_hc/skillc_hc/urls.py
@@ -16,7 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from myapp.views import hello
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path("hello/",hello,name = "welcome")
 ]
