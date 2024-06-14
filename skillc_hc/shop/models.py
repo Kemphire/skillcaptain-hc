5b23eb486c7b3cb6908930c128d4daa394c41729
Kartikey Shahi <hitmonlee@localhost.localdomain>
2024-06-14 22:43:57 +0530

    added admin interface for shop and product

--- skillc_hc/shop/models.py
+++ skillc_hc/shop/models.py
@@ -13,9 +13,20 @@ class Product(models.Model):
     name = models.CharField(max_length=100)
     price = models.DecimalField(max_digits=8, decimal_places=2)
     description = models.TextField(null=True, blank=True)
-    category = models.CharField(max_length=100)
-    created_at = models.DateTimeField(auto_now_add=True)
-    shops = models.ManyToManyField(Shop)
 
-    def __init__(self):
+    CATEGORIES = (
+        ("cosmetics","beauty product"),
+        ("stationery","study product"),
+    )
+
+    category = models.CharField(max_length=250,choices=CATEGORIES)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
         return f"{self.name = } : {self.price = }"
+    
+class ProductStore(models.Model):
+    store = models.ForeignKey(Shop,on_delete=models.CASCADE,related_name="store_of_product")
+    product = models.ForeignKey(Product,on_delete=models.CASCADE,related_name="product")
+
+
